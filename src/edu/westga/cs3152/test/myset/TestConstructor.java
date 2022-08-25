02dd80cdb6449700bca0ff5bee2e6b95c1955947
Dowee2 <93294849+Dowee2@users.noreply.github.com>
2022-08-25 09:16:52 -0400

    Add files via upload

---
+++ src/edu/westga/cs3152/test/myset/TestConstructor.java
@@ -0,0 +1,20 @@
+package edu.westga.cs3152.test.myset;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+//import edu.westga.cs3152.sets.MySet;
+
+public class TestConstructor {
+//	@Test
+//	public void testNewSetSizeIsZero() {
+//		MySet<String> aSet = new MySet<String>();
+//		assertEquals(0, aSet.size());
+//	}
+//
+//	@Test
+//	public void testNewSetIsEmpty() {
+//		MySet<Integer> aSet = new MySet<Integer>();
+//		assertTrue(aSet.isEmpty());
+//	}
+}
---
+++ src/edu/westga/cs3152/test/myset/TestMySetContains.java
@@ -0,0 +1,69 @@
+package edu.westga.cs3152.test.myset;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+//import edu.westga.cs3152.sets.MySet;
+
+public class TestMySetContains {
+
+//	@Test
+//	public void testOneElementShouldBeTrue() {
+//		MySet<String> set = new MySet<String>();
+//		
+//		set.add("A");
+//	
+//		assertTrue(set.contains("A"));
+//	}
+//	
+//	@Test
+//	public void testOneElementShouldBeFalse() {
+//		MySet<String> set = new MySet<String>();
+//
+//		set.add("B");
+//	
+//		assertFalse(set.contains("A"));
+//	}
+//	
+//	@Test
+//	public void testEmptySetShouldBeFalse() {
+//		MySet<String> set = new MySet<String>();
+//	
+//		assertFalse(set.contains("D"));
+//	}
+//	
+//	@Test
+//	public void testMultipleElementShouldBeTrue() {
+//		MySet<String> set = new MySet<String>();
+//		
+//		set.add("A");
+//		set.add("B");
+//		set.add("C");
+//		set.add("D");
+//	
+//		assertTrue(set.contains("B"));
+//	}
+//	
+//	@Test
+//	public void testMultipleElementShouldBeFalse() {
+//		MySet<String> set = new MySet<String>();
+//		
+//		set.add("A");
+//		set.add("B");
+//		set.add("C");
+//		set.add("D");
+//	
+//		assertFalse(set.contains("E"));
+//	}
+//
+//	@Test (expected=IllegalArgumentException.class) 
+//	public void testNullArgument() {
+//		MySet<String> set = new MySet<String>();
+//		String str = null; 
+//				
+//		set.add("A");
+//		
+//		set.contains(str);
+//	}
+}
